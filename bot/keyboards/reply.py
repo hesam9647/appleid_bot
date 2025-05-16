90a6552dca08864ce55f33b4d89440c6605a8edc
Hesam <hesammohmmdi0099@gmail.com>
2025-05-17 01:05:05 +0330

    first commit

--- bot/keyboards/reply.py
+++ bot/keyboards/reply.py
@@ -1,8 +1,10 @@
-# bot/keyboards/reply.py
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def admin_menu():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[],  # مهمه
+        resize_keyboard=True
+    )
     button1 = KeyboardButton("/stats")
     button2 = KeyboardButton("/addproduct")
     keyboard.add(button1, button2)
